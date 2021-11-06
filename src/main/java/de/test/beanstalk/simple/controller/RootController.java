d37d146331fe21ec0b98a2e5df29125169664f06
mark-herrmann <github@markherrmann.de>
2021-11-06 15:38:01 +0100

    remove _ from healthy

--- src/main/java/de/test/beanstalk/simple/controller/RootController.java
+++ src/main/java/de/test/beanstalk/simple/controller/RootController.java
@@ -9,7 +9,7 @@ public class RootController {
 
     @RequestMapping
     public ResponseEntity<String> awsHealthCheck(){
-        return ResponseEntity.ok("healthy_");
+        return ResponseEntity.ok("healthy");
     }
 
 }
